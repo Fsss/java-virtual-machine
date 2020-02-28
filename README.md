5011ec312f0ec493803d258e44df895a5acb4e39
Fsss <771069317@qq.com>
2020-02-29 03:12:59 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java Virtual Machine Project
+Java-virtual-machine is an exercise project for Fsss to learn JVM.
---
+++ src/main/java/com/fsss/study/java/virtual/machine/Application.java
@@ -0,0 +1,12 @@
+package com.fsss.study.java.virtual.machine;
+
+/**
+ * @author Fsss
+ * @date 2020/2/29
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
